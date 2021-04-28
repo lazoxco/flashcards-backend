49dacbe9c6ae71d7742b01e01e7b2754abcbee28
github username <github email address>
2021-04-28 18:47:13 -0400

    Added serializer gem.

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,2 +1,6 @@
 class FlashcardsController < ApplicationController
+  def index
+    flashcards = Flashcard.all
+    render json: flashcards.to_json(except: [:created_at, :updated_at], include: {topic: {only: [:name]}})
+  end
 end
