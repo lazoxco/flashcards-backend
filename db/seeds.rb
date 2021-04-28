198df03d235092db6895970f3a7a286f6dc16695
github username <github email address>
2021-04-28 18:31:52 -0400

    Seeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 ruby = Topic.create(name: "Ruby")
 html = Topic.create(name: "HTML")
 css = Topic.create(name: "CSS")
+
+flashcard1 = Flashcard.create(question: "What is Ruby?", answer: "Ruby is an interpreted, high-level, general-purpose programming language.", topic_id: ruby.id)
+
+flashcard2 = Flashcard.create(question: "What is HTML?", answer: "A standard markup language for documents designed to be displayed in a web browser.", topic_id: html.id)
+
+flashcard3 = Flashcard.create(question: "What is CSS?", answer: "A style sheet language used for describing the presentation of a document written in a markup language such as HTML. ", topic_id: css.id)
