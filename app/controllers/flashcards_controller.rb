941744e1333411a082e54944be47587703345dd9
github username <github email address>
2021-04-28 19:02:37 -0400

    Added index and show routes.

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -3,4 +3,9 @@ class FlashcardsController < ApplicationController
     flashcards = Flashcard.all
     render json: flashcards.to_json(except: [:created_at, :updated_at], include: {topic: {only: [:name]}})
   end
+
+  def show
+    flashcard = Flashcard.find(params[:id])
+    render json: flashcard.to_json(except: [:created_at, :updated_at], include: {topic: {only: [:name]}})
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,6 @@
 class TopicsController < ApplicationController
+  def index
+    topics = Topic.all
+    render json: topics.to_json(except: [:created_at, :updated_at], include: {flashcards: {only: [:question, :answer]}})
+  end
 end
