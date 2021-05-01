13f7cd9d13f878913347f2897b243e8f47db8b2b
github username <github email address>
2021-05-01 04:43:33 -0400

    Added create action.

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -8,4 +8,19 @@ class FlashcardsController < ApplicationController
     flashcard = Flashcard.find(params[:id])
     render json: flashcard.to_json(except: [:created_at, :updated_at], include: {topic: {only: [:name]}})
   end
+
+  def create
+    flashcard = Flashcard.new(flashcard_params)
+    if flashcard.save
+      render json: flashcard.to_json(except: [:created_at, :updated_at], include: {topic: {only: [:name]}})
+    else
+      render json: {error: "Sorry, could not create this flashcard."}
+    end
+  end
+
+  private
+
+  def flashcard_params
+    params.require(:flashcard).permit(:question, :answer, :topic)
+  end
 end
